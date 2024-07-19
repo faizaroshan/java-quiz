7b60f02ebb5274b5894062c4c1dce160494d4b1d
Faiza <faizarj50@gmail.com>
2024-07-19 03:12:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+
+
+ alert("Welcome To The Quiz");
+
+var  extension = prompt ("What is the file extension for JavaScript files?");
+if (extension === ".js") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (extension === ".js")
+{
+    alert("Incorrect!");
+}
+
+
+var  developed = prompt ("Which company developed JavaScript?");
+if ( developed === "Netscape") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else ( developed === "Netscape") 
+{
+    alert("Incorrect");
+}
+
+
+var keyword = prompt ("Which keyword is used to declare a variable in JavaScript?");
+if (keyword === "var, let, const") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (keyword === "var, let, const")
+{
+    alert("Incorrect");
+}
+
+
+var comment = prompt ("Which of the following is the correct syntax to write a comment in JavaScript?");
+if (comment === " // This is a comment") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (comment === "// This is a comment")
+{
+    alert("Incorrect");
+}
+
+
+var  type = prompt ("Which of the following is not a JavaScript data type?");
+if (type === "Float") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (type === "Float")
+{
+    alert("Incorrect");
+}
+
+
+var ifstatement = prompt ("Which of the following is the correct way to write an 'if' statement in JavaScript?");
+if (ifstatement === " if (i == 5)") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (ifstatement === "if (i === 5)")
+{
+    alert("Incorrect");
+}
+
+
+var value = prompt ("Which operator is used to assign a value to a variable in JavaScript?");
+if (value === "=") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (value === "=")
+{
+    alert("Incorrect!");
+}
+
+
+var keyword = prompt ("Which keyword is used to declare a constant in JavaScript?");
+if (keyword === "const") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (keyword === "const")
+{
+    alert("Incorrect!");
+}
+
+
+var  type = prompt ("What is the result of 'type of null' in JavaScript?");
+if (type === "object") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (type === "object")
+{
+    alert("Incorrect!");
+}
+
+
+var  element = prompt ("Which method is used to add a new element at the end of an array in JavaScript?");
+if (element === "push()") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (element === "push()")
+{
+    alert("Incorrect!");
+}
+
+
+var join = prompt ("Which method is used to join two or more arrays in JavaScript?");
+if (join === "concat()") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (join === "concat()")
+{
+    alert("Incorrect!");
+}
+
+
+var variable = prompt ("Which of the following is the correct way to create a JavaScript variable?");
+if (variable === "var myVariable;") {
+    alert("Congratulation! Your'e Answer is Correct.");
+}
+else (variable === "var myVariable;")
+{
+    alert("Incorrect!");
+}
+    alert("CONGRATULATION! YOU PASSED.");
